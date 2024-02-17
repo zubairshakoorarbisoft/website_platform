f90118ab92b18b0296ec5b48b28175dc55657edd
hamid-nz <hamidnazir.official@gmail.com>
2024-02-18 00:22:12 +0500

    dashboard done

--- website/dashboards/views.py
+++ website/dashboards/views.py
@@ -2,6 +2,9 @@ from django.views.generic import TemplateView
 
 from jobs.models import Job
 from clients.models import Client
+from products.models import Product
+from services.models import Service
+from technologies.models import Technology
 
 class DashboardsView(TemplateView):
     template_name = 'dashboard.html'
@@ -10,5 +13,8 @@ class DashboardsView(TemplateView):
         context = super().get_context_data(**kwargs)
         # Add additional context data if needed
         context['total_jobs'] = Job.objects.count()
-        context['testimonial_list'] = Client.objects.count()
+        context['total_testimonial'] = Client.objects.count()
+        context['total_product'] = Product.objects.count()
+        context['total_service'] = Service.objects.count()
+        context['total_technology'] = Technology.objects.count()
         return context
