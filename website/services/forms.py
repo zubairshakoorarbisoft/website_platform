f645f72d4b254b730d2536c91cd97a769ae322d4
hamid-nz <hamidnazir.official@gmail.com>
2024-03-08 16:39:18 +0500

    Services Incomplete

--- website/services/forms.py
+++ website/services/forms.py
@@ -4,10 +4,16 @@ from django.core.validators import MinValueValidator
 from datetime import datetime as dt
 
 from .widgets import SERVICES_FORM_WIDGET
-from .models import Service
-
+from .models import Service , SubServices
 class ServiceForm(forms.ModelForm):
     class Meta:
         model = Service
         fields = '__all__'
         widgets = SERVICES_FORM_WIDGET
+
+
+class SubServiceForm(forms.ModelForm):
+    class Meta:
+        model = SubServices
+        fields = '__all__'
+        # widgets = SUB_SERVICES_FORM_WIDGET
--- website/services/models.py
+++ website/services/models.py
@@ -9,3 +9,17 @@ class Service(models.Model):
     content= models.CharField(max_length=2000, blank=True)
     cover_image= models.FileField(upload_to= 'media/uploads', default='' , blank=True)
     url= models.SlugField(max_length=200, default="")
+    def __str__(self):
+        return self.service_name
+
+
+class SubServices(models.Model):
+    name= models.CharField(max_length=200, null=False)
+    category = models.ForeignKey(Service, on_delete=models.CASCADE)
+    disc= models.CharField(max_length=500, blank=True)
+    content= models.CharField(max_length=2000, blank=True)
+    image= models.FileField(upload_to= 'media/uploads' , default='', blank=True)
+    def __str__(self):
+        return self.name
+    
+    
